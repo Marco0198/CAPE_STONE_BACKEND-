1bf5ae2ee121bfa3063a9bcdefda1fd36bbc2772
Marco019816 <mtshimanga@oroafrica.com>
2022-11-16 09:42:03 +0200

    initial commit

---
+++ src/test/java/com/example/AZUREAD/AzureadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AZUREAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
